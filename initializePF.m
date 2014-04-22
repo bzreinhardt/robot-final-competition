2b7533d8990a51f65685e5094d9a3fecda1af723
BenR <bzreinhardt@gmail.com>
2014-04-22 13:31:38 -0400

    update localization functions ben is working on

---
+++ initializePF.m
@@ -0,0 +1,18 @@
+function particles = initializePF(waypoints)
+%INITIALIZEPF - create an initial set of state particles from possible
+%starting positions
+%%%%
+% INPUTS
+% waypoints - set of x,y coordinates where the robot could possibly start -
+% IN THE FORMAT THAT THEY'VE BEEN GIVEN - [Nx2]
+%%%
+% OUTPUTS
+% particles - set of [x;y;theta] particles. 4 particles/ waypoint, at
+% 0,pi/2,pi,3pi/2 - [3xN]
+particles = [];
+for i = 1:size(waypoints,1)
+    newParticles = [ones(1,4)*waypoints(i,1);ones(1,4)*waypoints(i,2);0:pi/2:3*pi/2];
+    particles = [particles,newParticles];
+end
+
+end
